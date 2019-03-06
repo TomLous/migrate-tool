e0a491120da30ed72954eb95b641c847db421454
Tom Lous <tomlous@gmail.com>
2019-03-06 12:28:41 +0100

    Fixes

--- build.sbt
+++
@@ -1,8 +0,0 @@
-name := "migrate-tool"
-
-version := "0.1"
-
-scalaVersion := "2.12.8"
-
-libraryDependencies += "com.47deg" %% "github4s" % "0.20.1"
-libraryDependencies += "com.typesafe" % "config" % "1.3.2"
---
+++ migrate.sh
@@ -0,0 +1,50 @@
+#!/usr/bin/env bash
+
+#set -x
+
+clientId=$1
+secret=$2
+projectKey=$3
+accountName=$4
+userName=$5
+userEmail=$6
+
+ORIGIN=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
+
+echo $ORIGIN
+cd $ORIGIN
+rm -rf repos
+mkdir -p repos
+cd repos
+
+
+while read gitrepo; do
+  accessToken=$(curl -s -X POST -u "$clientId:$secret" https://bitbucket.org/site/oauth2/access_token -d grant_type=client_credentials | jq -r ".access_token")
+  gitname=$(echo "$gitrepo" | sed  's/.*\/\(.*\)\.git/\1/g')
+  cd $ORIGIN/repos || { echo 'Failed to change to repos dir' ; exit 1; }
+  echo -n "Cloning $gitrepo to $gitname ... "
+  git clone --depth 1 --quiet $gitrepo $gitname
+  echo "OK"
+  echo -n "Creating bitbucket repo ... "
+  httpStatus=$(curl -s -o /dev/null -w "%{http_code}"  -H "Authorization: Bearer $accessToken" -H "Content-Type: application/json" "https://api.bitbucket.org/2.0/repositories/$accountName/$gitname" -d '{"scm":"git","project":{"key":"'$projectKey'"},"is_private":"true"}')
+  if [[ $httpStatus -eq "200" ]]; then
+      echo "Ok"
+      cd $ORIGIN/repos/$gitname || { echo 'Failed to change to repo' ; exit 1; }
+      rm -rf .git
+      git init
+      git config user.name $userName
+      git config user.email $userEmail
+      git add . cd > /dev/null
+      git commit -m "First from shallow clone" --quiet
+    #  git filter-branch -- --all
+      git remote add origin git@bitbucket.org:$accountName/$gitname.git || { echo 'Failed to change the origin' ; exit 1; }
+    #  git remote set-url origin git@bitbucket.org:$accountName/test_$gitname.git || { echo 'Failed to change the origin' ; exit 1; }
+      git push -u origin master --quiet
+   else
+     echo "Skip"
+   fi
+
+
+
+
+done < ../source-repos
